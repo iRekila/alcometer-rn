c5a394cf519b383dc98bce25cd97026323de04de
iRekila <ilkka.rekila@gmail.com>
2023-02-18 22:06:33 +0200

    added alert

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Switch } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Switch, Alert } from 'react-native';
 import styles from './styles/Styles';
 
 const BloodAlcoholCalculator = () => {
@@ -11,6 +11,12 @@ const BloodAlcoholCalculator = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const calculate = () => {
+
+    if (!weight) {
+      Alert.alert('Error!', 'Please enter your weight.');
+      return;
+    }
+
     const litres = bottles * 0.33;
     const grams = litres * 8 * 4.5;
     const burning = weight / 10;
@@ -65,10 +71,10 @@ const BloodAlcoholCalculator = () => {
         <Text style={styles.buttonText}>Calculate</Text>
       </TouchableOpacity>
       <Text style={textStyles}>Blood alcohol level:</Text>
-      <Text style={[styles.result, isDarkMode ? styles.resultDark : null, 
+      <Text style={[styles.result, isDarkMode ? styles.resultDark : null,
       grams < 1 ? styles.resultGreen : grams >= 1 && grams <= 2 ? styles.resultOrange : styles.resultRed]}>
-      {grams.toFixed(2)}
-    </Text>
+        {grams.toFixed(2)}
+      </Text>
       <View style={styles.toggleContainer}>
         <Text style={textStyles}>Dark mode:</Text>
         <Switch trackColor={{ true: '#f69edf', false: 'grey' }} value={isDarkMode} onValueChange={setIsDarkMode} />
